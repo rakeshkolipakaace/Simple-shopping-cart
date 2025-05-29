4aac5226958deea5613761ccc526024f4d69e3f1
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-29 19:53:10 +0530

    data props

--- react-shopping/src/shoppingfolder/pages/Mainpage.jsx
+++ react-shopping/src/shoppingfolder/pages/Mainpage.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import Collections from '../components/Collections'
 import Footer from '../components/Footer'   
 
+import {Gents,Ladies} from '../data'
+import WomenCollections from '../components/WomenCollections'
+
 const Mainpage = () => {
+    const [gentsFashion, setgentsFashion] = useState(Gents)
+    const [LadiesFashion, setLadiesFashion] = useState(Ladies)
   return (
     <>
     <div>
       <Header/>
       <Banner/>
-      <Collections/>
+      <Collections genstFashion={gentsFashion}/>
+      <WomenCollections LadiesFashion={LadiesFashion} ></WomenCollections>
       <Footer/>
 
      
