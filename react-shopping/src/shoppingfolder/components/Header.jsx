f1e0152556af22fdf831993fc375dd1e33028c2d
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-29 15:19:33 +0530

    main code

--- react-shopping/src/shoppingfolder/components/Header.jsx
+++ react-shopping/src/shoppingfolder/components/Header.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <>
+
+    <div className="left">
+        <div className="title"><h2>Shopping Mall</h2></div>
+    </div>
+    <div className="center">
+        <ul>
+            <li>Women</li>
+            <li>Men</li>
+            <li>Children</li>
+            <li>Beauty</li>
+        </ul>
+    </div>
+    <div className="search">
+        <input type="text" placeholder='serach'></input>
+    </div>
+    <div className="right">
+        <div className="sign">Sign In/Signup</div>
+        <div className="cart">Cart</div>
+    </div>
+   </>
   )
 }
 
