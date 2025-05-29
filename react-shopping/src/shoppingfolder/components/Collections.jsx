9d0ae1ca45c392380ae20073b75627de695cbe6d
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-29 19:50:59 +0530

    Mens collection

--- react-shopping/src/shoppingfolder/components/Collections.jsx
+++ react-shopping/src/shoppingfolder/components/Collections.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const Collections = () => {
+const Collections = (props) => {
+  const {title, image1, image2, image3, image4, image5, image6, price1, price2, price3, price4, price5, price6} = props.genstFashion
   return (
-    <div>Collections</div>
+    <div className='collectionSection'> 
+      <h2>{title}</h2>
+
+      <div className="menImages">
+        <img src={image1} alt="image1"></img>
+        <img src={image2} alt="image2"></img>
+        <img src={image3} alt="image3"></img>
+        <img src={image4} alt="image4"></img>
+        <img src={image5} alt="image5"></img>
+        <img src={image6} alt="image6"></img>
+      </div>
+      
+
+    </div>
   )
 }
 
