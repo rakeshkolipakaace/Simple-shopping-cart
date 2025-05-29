9e0171a88839b69fa77ce8fd5f4dfffbea6a1255
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-29 14:50:55 +0530

    components and pages

---
+++ react-shopping/src/shoppingfolder/pages/Mainpage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import Collections from '../components/Collections'
+import Footer from '../components/Footer'   
+
+const Mainpage = () => {
+  return (
+    <>
+    <div>
+      <Header/>
+      <Banner/>
+      <Collections/>
+      <Footer/>
+
+     
+    </div>
+
+    
+    </>
+  )
+}
+
+export default Mainpage
