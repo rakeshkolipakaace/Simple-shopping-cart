2f0ff2143f9ffe397f7798f60dfc2a85948f463a
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-09-29 23:58:17 +0530

    Header.jsx

--- react-Redux/React-Redux/src/Components/Home.jsx
+++ react-Redux/React-Redux/src/Components/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Header />
+    </>
+  );
+};
 
-export default Home
+export default Home;
