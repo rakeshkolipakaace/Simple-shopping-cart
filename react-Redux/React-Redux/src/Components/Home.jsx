a148d743017940e4e101e7815ec2896e005548e9
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-09-30 23:09:02 +0530

    navigation import

--- react-Redux/React-Redux/src/Components/Home.jsx
+++ react-Redux/React-Redux/src/Components/Home.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
 import Sliders from "./Sliders";
 
 const Home = () => {
   return (
     <>
-      <Header />
       <Sliders />
-      <Footer />
     </>
   );
 };
