6a5b00da1ce2a19f670be52710414183bc41b92f
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-09-30 23:08:01 +0530

    navigation

--- react-Redux/React-Redux/src/App.jsx
+++ react-Redux/React-Redux/src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import Home from "./Components/Home";
+import About from "./Components/About";
+
 function App() {
   return (
-    <>
-      <Home />
-      
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
