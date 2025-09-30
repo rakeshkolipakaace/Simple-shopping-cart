e480b3a3c4cf05f1d826894e9f3a625794b12895
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-09-30 23:09:52 +0530

    about page

---
+++ react-Redux/React-Redux/src/Components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container py-4">
+      <h1>About</h1>
+      <p className="lead m-0">This is a placeholder About page.</p>
+    </div>
+  );
+};
+
+export default About;
+
+
