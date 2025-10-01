682afca4bc23899743541768a92640425d3e7cef
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-10-01 22:11:33 +0530

    redux connect to react

--- react-Redux/React-Redux/src/main.jsx
+++ react-Redux/React-Redux/src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { Provider } from "react-redux";
+import store from "./redux/store.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>
 );
