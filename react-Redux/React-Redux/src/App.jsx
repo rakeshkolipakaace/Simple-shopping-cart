67a3dd432ca0d73252636694db17defac78002bb
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-10-01 22:10:06 +0530

    posts route

--- react-Redux/React-Redux/src/App.jsx
+++ react-Redux/React-Redux/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Post from "./Components/Post";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Post />} />
       </Routes>
       <Footer />
     </BrowserRouter>
