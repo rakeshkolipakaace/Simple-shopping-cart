3f9fb59769ccb9e8a9a7ad6ac6289301e196bc93
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-11-01 13:46:41 +0530

    is active

--- navigation/src/Components/Navbar.jsx
+++ navigation/src/Components/Navbar.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-<>
+    <>
+      <div style={{ background: "pink", padding: "10px", textAlign: "center" }}>
+        <NavLink
+          style={({ isActive }) => ({
+            color: isActive ? "white" : "blue",
+            backgroundColor: isActive ? "green" : "transparent",
+            padding: "5px 10px",
+            borderRadius: "5px",
+            textDecoration: "none",
+          })}
+          to="/"
+        >
+          Home
+        </NavLink>
 
-<div style={{background:'pink',padding:'10px',textAlign:'center'}}>
-    <Link style={{margin:'10px'}} to="/">Home</Link>
-<Link style={{margin:'10px'}} to="/about">About</Link>  
-<Link style={{margin:'10px'}} to="/contact">Contact</Link>
-</div>
+        <NavLink
+          style={({ isActive }) => ({
+            color: isActive ? "white" : "blue",
+            backgroundColor: isActive ? "green" : "transparent",
+            padding: "5px 10px",
+            borderRadius: "5px",
+            textDecoration: "none",
+          })}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => ({
+            color: isActive ? "white" : "blue",
+            backgroundColor: isActive ? "green" : "transparent",
+            padding: "5px 10px",
+            borderRadius: "5px",
+            textDecoration: "none",
+          })}
+          to="/contact"
+        >
+          Contact
+        </NavLink>
+      </div>
+    </>
+  );
+};
 
-
-</>  )
-}
-
-export default Navbar
+export default Navbar;
