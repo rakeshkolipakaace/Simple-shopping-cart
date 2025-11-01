22bd9215b58ffaeee48fdf82a4bc19acb2cae3aa
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-11-01 13:31:39 +0530

    Navbar.jsx

---
+++ navigation/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+<>
+<Link to="/">Home</Link>
+<Link to="/about">About</Link>  
+<Link to="/contact">Contact</Link>
+
+
+</>  )
+}
+
+export default Navbar
